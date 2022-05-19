6c837237c88248c94562d57ca99d9c678b71c391
Taylor Guenthner <wtguenthner@gmail.com>
2022-05-19 11:18:19 -0400

    Added local storage for storing initials and highscores

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ var main = document.querySelector("main");
 var message = document.getElementById("message");
 var highscores = document.getElementById("highscores");
 
-
+//Sets an array for questions. Q stands for questions, A stands for available answers, C stands for correct answer
 var questions = [
     {
         Q: "What does HTML stand for?",
@@ -43,20 +43,18 @@ document.getElementById("start").addEventListener("click", handleQuiz);
 
 //Starts the quiz and timer
 function handleQuiz(){
-
     handleQuestion();
     clockID= setInterval(handleTime, 1000);
-
 };
 
 function handleQuestion(){
-    
     qi++
     //Stops questions and ends the quiz once the iterator is larger than the number of questions available.
     if(qi >4){
         endQuiz();
         return
     }
+
     //Adds questions to the page
     let {Q,A,C} = questions[qi];
     main.innerHTML = `<h1>${Q}</h1>`;
@@ -92,13 +90,10 @@ function handleQuestion(){
         handleQuestion();
        time = time-10;
    }
-   
 }
 
 function handleTime(){
-
     time--;
-   
     document.getElementById("time").innerHTML = time;
 //Ends quiz if timer is 0 or less
     if(time <= 0){    
@@ -107,7 +102,6 @@ function handleTime(){
         document.getElementById("time").innerHTML = time;
         clearInterval(clockID);
     }
-
 }
 
 //Ends quiz and shows user their score. Creates input box for user's initials.
@@ -120,17 +114,38 @@ function endQuiz(){
 }
 
 function addScore(points){
-let initials = document.getElementById("initials").value; 
-//  window.localStorage.setItem("hsInitials", initials);
-//  window.localStorage.setItem("hsPoints", points);
-main.textContent = " ";
-var listInitials=[initials];
-var listScore = [points]
-window.localStorage.setItem("hsInitials", JSON.stringify(listInitials));
-window.localStorage.setItem("hsPoints", JSON.stringify(listScore));
-console.log(listInitials + listScore)
-// highscores.innerHTML+= `<br></br> <li>` + window.localStorage.getItem("hsInitials", JSON.parse(initials)) + window.localStorage.getItem("hsPoints", JSON.parse(points)) + `</li>`
+var initials = document.getElementById("initials").value; 
+// main.textContent = " ";
+//Set initials and points to an array
+if(localStorage.getItem('initial') == null){
+    localStorage.setItem('initial', '[]')
+}
+    if(localStorage.getItem('points')==null){
+    localStorage.setItem('points', '[]');
+    }
+//Stores local storage of points into a variable and parses it
+var pointList = JSON.parse(localStorage.getItem('points'));
+//Add the new score to the array of points
+pointList.push(points);
+localStorage.setItem('points', JSON.stringify(pointList));
 
-highscores.setAttribute("class", "show");
-highscores.innerHTML += `<br><button id="goBack"><a href="./index.html">Go Back</a></button>`
+//Stores local storage of initials into a variable and parses it
+var initialList = JSON.parse(localStorage.getItem('initial'));
+//Adds the new initials to the array of initials
+initialList.push(initials);
+localStorage.setItem('initial', JSON.stringify(initialList))
+showHighScores();
+}
+
+function showHighScores(){
+    //Resets content content of the main tag and the highscore list
+    main.textContent = " ";
+    highscores.textContent = " ";
+    //Displays the highscores
+    highscores.setAttribute("class", "show");
+        highscores.innerHTML+= `<h2>High Scores</h2>`
+        highscores.innerHTML+= `<li>` + JSON.parse(localStorage.getItem('initial')) + `</li>`;
+        highscores.innerHTML+= `<br><li>` + JSON.parse(localStorage.getItem('points')) + `</li>`
+        //Adds button to return to beginning of quiz
+        highscores.innerHTML += `<br><button id="goBack"><a href="./index.html">Go Back</a></button>`
 }
